66df4d1655d3dc5316ed695bc29b2aba1a7b969b
Ali1715 <tacoomaturanoalison@gmail.com>
2023-02-27 07:56:22 -0400

    actualizacion proyecto

--- app/src/main/java/com/arbolB/arbolexpresiones/MainAct.java
+++ app/src/main/java/com/arbolB/arbolexpresiones/MainAct.java
@@ -15,12 +15,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainAct extends AppCompatActivity {
+    Button generar;
 
     // Button btnIniciar;
     EditText expresion;
     Button btncalcular;
     EditText ediTvalorn;
     TextView resultado;
+    TextView mostrar;
+    EditText edtif;
+    EditText edtif2;
+    TextView edtif3;
     ArbolExpresionesClass a = new ArbolExpresionesClass();
 
     @Override
@@ -30,16 +35,58 @@ public class MainAct extends AppCompatActivity {
 
         ediTvalorn = (EditText) findViewById(R.id.ediT_valor_n);
         expresion = (EditText) findViewById(R.id.EdTexpresion);
+        edtif = (EditText) findViewById(R.id.editText_if);
+        edtif2 = (EditText) findViewById(R.id.editText_if2);
+        edtif3 = (TextView) findViewById(R.id.textView8);
         btncalcular = (Button) findViewById(R.id.btnCalcular);
         resultado = (TextView) findViewById(R.id.Textview_resultado);
+        mostrar = (TextView) findViewById(R.id.textView4);
+        generar=(Button) findViewById(R.id.buttongen);
+
+        generar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Python(1,ediTvalorn.getText().toString(),expresion.getText().toString(),edtif.getText().toString(),edtif2.getText().toString(),resultado.getText().toString(),mostrar);
+            }
+
+        });
 
         btncalcular.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
 
 
                 a.Arbol(a.AcomodarExpresion(expresion.getText().toString(), ediTvalorn.getText().toString()));
                 double resp = a.Resultado();
+                /*for (int i=0; i<=edtif.getText().length();i++){
+
+
+                        Character ifn=edtif.getText().charAt(i);
+                        switch (ifn) {
+                            case '>':
+                                if(Double.parseDouble(edtif2.getText().toString())<resp)
+                                    resultado.setText(String.valueOf(resp));
+
+                                break;
+                            case '<':
+                                if(Double.parseDouble(edtif2.getText().toString())>resp)
+                                    resultado.setText(String.valueOf(resp));
+
+                                    break;
+                            case '=':
+                                if(resp==Double.parseDouble(edtif2.getText().toString()))
+                                    resultado.setText(String.valueOf(resp));
+
+                                break;
+                            default:
+                                ;
+                                break;
+
+                        }
+
+                }*/
+
                 resultado.setText(String.valueOf(resp));
 
 
@@ -47,6 +94,7 @@ public class MainAct extends AppCompatActivity {
         });
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,9 +105,34 @@ public class MainAct extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.btncode:
+                Toast.makeText(getBaseContext(), "Code", Toast.LENGTH_SHORT).show();
+
+                break;
             case R.id.btnadd:
-                Toast.makeText(getBaseContext(), "Diagramas", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "Code", Toast.LENGTH_SHORT).show();
+                break;
+              //  startActivity(intent);
+
+
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void Python(int i, String n,String x,String m,String e,String r, TextView textView) {
+
+        switch (i) {
+
+                 case 1:
+                textView.append(n + "\n");     //solo muestra los valores de
+                      textView.append(x + "\n");
+                      textView.append("if " + "c" + m + e + ":"+ "\n");
+                     textView.append("print("+r +  ")" +"\n");
+                     break;
+
+                 default:
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/arbolB/arbolexpresiones/code.java
@@ -0,0 +1,17 @@
+package com.arbolB.arbolexpresiones;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class code extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gencode);
+
+
+    }
+}
